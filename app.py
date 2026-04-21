d68f8bca38ff975e6544c09209094000fc39fdcd
Azepuo <azepuo11@gmail.com>
2026-04-21 19:43:10 +0200

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version Finale - Pipeline DevOps OK (Docker + K8S) - Tout est automatique !"
+    return "Version 2 - Déployée automatiquement"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
