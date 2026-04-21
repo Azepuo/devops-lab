c2caf3a8c2bbf179ecb01e95ab9b8d223c949b56
Azepuo <azepuo11@gmail.com>
2026-04-21 19:37:46 +0200

    Final verification push

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version Finale - Pipeline DevOps Complet (Docker + Jenkins + K8S) réussi !"
+    return "Version Finale - Pipeline DevOps OK (Docker + K8S) - Tout est automatique !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
