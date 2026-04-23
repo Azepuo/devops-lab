30cbb917329822869b38e9421c41bccb94e220e4
Azepuo <azepuo11@gmail.com>
2026-04-23 23:20:39 +0200

    Final Demo: Version 60

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 56 - Déployée automatiquement yassine"
+    return "Version 60 - FINALE DÉMO - SUCCESS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
