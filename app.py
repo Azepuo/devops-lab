e529e4ddb16daa8b0d86acd5131dad7fef478eed
Azepuo <azepuo11@gmail.com>
2026-04-24 10:10:52 +0200

    result final

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bienvenue dans le module DevOps (Docker, K8s, Jenkins et ngrok)  version 21"
+    return "  version final"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
